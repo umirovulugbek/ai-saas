60fb000d1ea4e96e9e12dc9c4e102a60fc474d37
umirovulugbek <umirovulugbek3@gmail.com>
2024-03-10 23:23:03 +0500

    add dashboard

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from 'lucide-react';
 import { Montserrat } from 'next/font/google';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const montserrat = Montserrat({
 	weight: '600',
@@ -10,7 +11,7 @@ const montserrat = Montserrat({
 });
 
 const routes = [
-	{ label: 'Dashboard', icon: LayoutDashboard, href: 'dashboard', color: 'text-sky-500' },
+	{ label: 'Dashboard', icon: LayoutDashboard, href: '/dashboard', color: 'text-sky-500' },
 	{
 		label: 'Conversation',
 		icon: MessageSquare,
@@ -49,6 +50,7 @@ const routes = [
 ];
 
 const SideBar = () => {
+	const pathname = usePathname();
 	return (
 		<div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
 			<div className='px-3 py-2 flex-1'>
@@ -61,7 +63,10 @@ const SideBar = () => {
 						<Link
 							key={item?.href}
 							href={item?.href}
-							className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'
+							className={cn(
+								'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+								pathname === item?.href ? 'text-white bg-white/10' : ' text-zinc-400'
+							)}
 						>
 							<div className='flex items-center flex-1'>
 								<item.icon className={cn('h-5 w-5 mr-3', item?.color)} />
