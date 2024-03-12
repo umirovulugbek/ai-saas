1666fd044c557b924411e32657322bb7b7e94636
umirovulugbek <umirovulugbek3@gmail.com>
2024-03-12 20:53:28 +0500

    landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,18 +1,13 @@
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { LandingContent } from '@/components/landing-content';
+import { LandingHero } from '@/components/landing-hero';
+import { LandingNavbar } from '@/components/landing-navbar';
 
 const LandingPage = () => {
 	return (
-		<div>
-			Landing page (Unprotected)
-			<div>
-				<Link href={'/sign-in'}>
-					<Button>Login</Button>
-				</Link>
-				<Link href={'/sign-up'}>
-					<Button>Register</Button>
-				</Link>
-			</div>
+		<div className='h-full '>
+			<LandingNavbar />
+			<LandingHero />
+			{/* <LandingContent /> */}
 		</div>
 	);
 };
